6930e4978515bad041f62d46e869964ccde8496e
dongqinglin <1745509482@qq.com>
2021-04-20 17:10:05 +0800

    nginx and service config

--- src/app/commons/app.share.module.ts
+++ src/app/commons/app.share.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { IonicModule } from "@ionic/angular";
 
 // BrowserAnimationsModule error repeat dependency BrowserModule
